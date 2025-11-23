135145d0dfbaf81c6969417cf054ae786342c46a
giuseppe.falcone <giuseppe.falcone@lutech.it>
2025-11-23 19:05:58 +0100

    Implemented redis cache configuration

---
+++ src/main/java/com/certimetergroup/easycv/bffwebapp/config/cache/RedisCacheConfig.java
@@ -0,0 +1,56 @@
+package com.certimetergroup.easycv.bffwebapp.config.cache;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("domains", defaultConfig.entryTtl(Duration.ofHours(24)));
+        configMap.put("domain", defaultConfig.entryTtl(Duration.ofHours(24)));
+        configMap.put("domainOption", defaultConfig.entryTtl(Duration.ofHours(24)));
+        configMap.put("user", defaultConfig.entryTtl(Duration.ofMinutes(60)));
+        configMap.put("curriculum", defaultConfig.entryTtl(Duration.ofMinutes(30)));
+        configMap.put("userLightDto", defaultConfig.entryTtl(Duration.ofMinutes(15)));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(configMap)
+                .build();
+    }
+}
